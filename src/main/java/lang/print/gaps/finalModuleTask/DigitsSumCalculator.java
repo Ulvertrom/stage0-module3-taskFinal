9a706e9a7c0d3082c2e33a4922d797b113109769
ulvertrom <ulvertrom@gmail.com>
2023-02-12 12:14:44 +0400

    6 progs added

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int digitOne = (number / 1000),
+                digitTwo = (number / 100 - digitOne * 10),
+                digitThree = (number/10 - digitOne * 100 - digitTwo * 10),
+                digitFour = (number - digitOne * 1000 - digitTwo * 100 - digitThree * 10);
+        System.out.println(digitOne + digitTwo + digitThree + digitFour);
     }
+
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int digitOne = (number / 100),
+                digitTwo = (number / 10 - digitOne * 10),
+                digitThree = (number - digitOne * 100 - digitTwo * 10);
+        System.out.println(digitOne + digitTwo * 10 + digitThree * 100);
     }
+
 }
